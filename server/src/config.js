b3436e9c3b2fc39a736e78ba493d32f89a6d2759
anhelina-hordiienko <anhelina.hordiienko@gmail.com>
2020-01-14 16:23:04 +0200

    add basic email sending

---
+++ server/src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    USER: 'test.m.a.gang.48@gmail.com', 
+    PASS: 'qycqnjcenoncpifk'
+}
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,43 +1,55 @@
 import React from 'react';
+import axios from 'axios';
 
 class ContactForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            
-        };
-    }
-    
-    changeHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        this.setState({[nam]: val});
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+
+    };
+  }
+
+  changeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    axios({
+      method: "POST", 
+      url:"http://localhost:3002/send", 
+      data:  this.state,
+    }).then((response)=>{
+      if (response.data.status === 'success'){
+        alert("Message Sent."); 
+        this.resetForm();
+      }else if(response.data.status === 'fail'){
+        alert("Message failed to send.");
       }
+    })
+  }
+  resetForm(){
+    this.setState({name: '', email: '', message: ''});
+ }
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting " + this.state.name);
-    }
-
-    render() {
-      return(
-        <form className="contact--form" method="post" action="/" name="getintouch" onSubmit={this.submitHandler}>
-            <input type="hidden" name="form-name" value="getintouch" />
-            <input type="hidden" name="bot-field" />
-            <input type="text" name="name" className="contact--form-input" placeholder="Name" required value={this.state.name} onChange={this.changeHandler} />
-            <input type="email" name="email" className="contact--form-input" placeholder="E-mail" required value={this.state.email} onChange={this.changeHandler} />
-            <textarea name="message" className="contact--form-input" placeholder="Message" value={this.state.message} onChange={this.changeHandler} />
-            <button type="submit" className="link-button dark wide">Send</button>
-        </form>
-      );
-    }
-    
-    handleSubmit(event) {
-
-    }
+  render() {
+    return (
+      <form className="contact--form" method="post" action="/" name="getintouch" onSubmit={this.submitHandler}>
+        <input type="hidden" name="form-name" value="getintouch" />
+        <input type="hidden" name="bot-field" />
+        <input type="text" name="name" className="contact--form-input" placeholder="Name" required value={this.state.name} onChange={this.changeHandler} />
+        <input type="email" name="email" className="contact--form-input" placeholder="E-mail" required value={this.state.email} onChange={this.changeHandler} />
+        <textarea name="message" className="contact--form-input" placeholder="Message" value={this.state.message} onChange={this.changeHandler} />
+        <button type="submit" className="link-button dark wide">Send</button>
+      </form>
+    );
+  }
 }
 
 export default ContactForm;
